027ea072774b2ee3e18f8ff896422e087ba5bae4
김지윤 <thelovely1125@naver.com>
2020-11-17 17:26:13 +0900

    master branch change a code

--- Hello world/src/HelloWorld.java
+++ Hello world/src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Kim jiyoun");
+		System.out.println("20192737");
 
 	}
 
